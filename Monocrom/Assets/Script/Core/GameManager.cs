3d4388596079a11e2a45f60312fa991d7e7fb34c
MestreSol <mb.muitobosta@gmail.com>
2024-11-04 23:10:49 -0300

    Add Project Content

---
+++ Monocrom/Assets/Script/Core/GameManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour{
+    public static GameManager Instance;
+    private void Awake(){
+        Instance = this;
+    }
+    
+}
---
+++ Monocrom/Assets/Script/Entity/Entity.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+  public string Name;
+  public int Alma;
+  public int Carisma;
+  public int Intuição;
+  public int Percepcao;
+  public int Razao;
+  public int Violencia;
+}
